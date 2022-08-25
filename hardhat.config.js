883f47bd9e2e201e1a81e45cce415993ca9eb404
Borislav Stoyanov <bostoy@protonmail.com>
2022-08-25 15:39:40 +0300

    ✨ Get the AAVE LendingPool contract address

--- hardhat.config.js
+++ hardhat.config.js
@@ -85,6 +85,9 @@ module.exports = {
             {
                 version: "0.4.19",
             },
+            {
+                version: "0.6.12",
+            },
         ],
     },
     mocha: {
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,11 +1,35 @@
+const { getNamedAccounts, ethers } = require("hardhat")
 const { getWeth } = require("./getWeth")
 
 ;(async () => {
     //the protocol treats everything as an ERC20
     await getWeth()
+    const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
+    console.log("Lending Pool Address : ", lendingPool.address)
 })()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error)
         process.exit(1)
     })
+
+/**
+ * This function returns the lendingPool contract after getting it from the lendingPoolAddressesProvider
+ * @param {*} account - the deployer
+ * @returns lendingPool contract
+ */
+const getLendingPool = async (account) => {
+    /**
+     * We have to use the ILendingPoolAddressesProvider to get the LendingPool address
+     */
+    const lendingPoolAdressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+
+    const lendingPoolAddress = await lendingPoolAdressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
